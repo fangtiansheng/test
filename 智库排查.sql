16d0ec90e125fc2d69b5e15e2fd851a47b0078ce
房天生 <vipfts@qq.com>
2022-03-14 18:11:51 +0800

    :+1:

---
+++ 智库排查.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT run_id from think_tank_detail2 WHERE website = "Economic Growth Center, Yale University (EGC)";
+
+select * from think_tank_detail2 WHERE website = "Daiwa Institute of Research Group(DIR)";
+UPDATE think_tank_detail2 SET website = "" WHERE website = "";
+select * from think_tank_detail2 where run_id = "337735467459940352";
